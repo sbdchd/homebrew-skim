deb37dbf212bbd545c292468a3836eb8271a65fc
Marco Esposito <marco.esposito@tum.de>
2017-10-22 12:28:47 +0200

    Fixed sha hash for .tar.gz instead of .tar

--- Formula/skim.rb
+++ Formula/skim.rb
@@ -3,7 +3,7 @@ class Skim < Formula
   desc "Fuzzy finder written in Rust"
   homepage "https://github.com/lotabout/skim"
   url "https://github.com/lotabout/skim/releases/download/#{version}/skim-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "252a8dfe1f5dfb23bfc14be2f6942634e550a0662d6d06f593d7dd662634a653"
+  sha256 "d3e62c8927f5d671e8a0516dfae88ed706a7f227a0e0236ae16447a132dea5d0"
 
   def install
     bin.install "sk"
