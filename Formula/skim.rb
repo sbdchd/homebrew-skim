67330c83d9fbd29c1dfe0ba6246ab7a13bde5c50
Steve Dignam <steve@dignam.xyz>
2018-11-17 19:16:08 -0500

    update to version 0.5.2

--- Formula/skim.rb
+++ Formula/skim.rb
@@ -1,9 +1,9 @@
 class Skim < Formula
-  version "v0.3.0"
+  version "v0.5.2"
   desc "Fuzzy finder written in Rust"
   homepage "https://github.com/lotabout/skim"
   url "https://github.com/lotabout/skim/releases/download/#{version}/skim-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "d3e62c8927f5d671e8a0516dfae88ed706a7f227a0e0236ae16447a132dea5d0"
+  sha256 "9e66bb84ca0dde00e7b067d01168a82c7da175e6f485a6d6084ea30354152190"
 
   def install
     bin.install "sk"
